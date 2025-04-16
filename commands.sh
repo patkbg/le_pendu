654da5926f9a836b43dec1807b2550015e55e7cf
PatcHer448 <patkabongo3@gmail.com>
2025-04-16 16:03:34 +0200

    [~] Modification du file commands.sh : Ajout de commentaire

--- commands.sh
+++ commands.sh
@@ -1,6 +1,16 @@
+# Initialiser un dépôt Git local.
 git init
+# Ajouter tous les fichiers modifiés au staging.
 git add .
+# Créer un premier commit avec un message.
 git commit -m "1er commit"
+# Renommer la branche principale en main.
 git branch -M main
-git remote add origin https://github.com/patkbg/azer.git
-git push --set-upstream origin main     
+# Configurer l'URL du dépôt distant GitHub.
+git remote set-url origin https://...
+# Pousser la branche main vers le dépôt distant et définir la relation par défaut.
+git push --set-upstream origin main
+# Pousser les changements vers GitHub sur la branche main.
+git push -u origin main
+# Vérifier les dépôts distants configurés.
+git remote -v
